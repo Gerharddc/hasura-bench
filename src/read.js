c5227fd7bc7a3ba8782311602dd622cedd7d17da
Gerhard de Clercq <gerharddeclercq@outlook.com>
2020-01-29 13:25:56 +0200

    Added env vars

--- src/read.js
+++ src/read.js
@@ -4,7 +4,7 @@ const app = express();
 const port = 3000;
 
 const query = `
-  query Querrer {
+  query TestQuery {
     Test_aggregate {
         aggregate {
           avg {
@@ -20,21 +20,29 @@ const query = `
 
 async function run() {
   startTime = new Date();
-  const arr = [];
-  for (let i = 0; i < 10; i++) {
-    const res = await request("http://hasura:8080/v1/graphql", query);
-    arr.push(res);
+
+  let ran = 0;
+  for (let j = 0; j < process.env.COUNT / 100; j++) {
+    const arr = [];
+
+    for (let i = 0; i < 100; i++) {
+      const res = await request(process.env.URL, query);
+      arr.push(res);
+      ran++;
+    }
+
+    await Promise.all(arr);
   }
-  await Promise.all(arr);
+
   endTime = new Date();
 
   var timeDiff = endTime - startTime;
   timeDiff /= 1000;
-  return timeDiff;
+  return timeDiff / ran;
 }
 
 app.get("/", async (req, res) => {
-  res.send("Time: " + (await run()));
+  res.send("RPM: " + (await run()));
 });
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
